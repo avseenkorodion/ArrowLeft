730f3f45b830e1a3199abb8a4669dbe3420229dd
Cari Peck <152903975+avseenkorodion@users.noreply.github.com>
2024-02-04 17:18:04 +0100

    Create ArrowLeft.swift

---
+++ ArrowLeft.swift
@@ -0,0 +1,11 @@
+ case 'ArrowDown':
+            player.y += player.speed;
+            break;
+          case 'ArrowLeft':
+            player.x -= player.speed;
+            break;
+          case 'ArrowRight':
+            player.x += player.speed;
+            break;
+        }
+      });
